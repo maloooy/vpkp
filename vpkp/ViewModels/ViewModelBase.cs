322457f356e8e1ea6343d517a3768e586e06c88c
Mikhail <basargin123@gmail.com>
2022-05-24 21:02:26 +0700

    check

---
+++ vpkp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vpkp.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
